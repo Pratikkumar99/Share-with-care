b089c79177f831ed4afb47b7177dc481d1199219
Pratik Kumar <pratikk12093@gmail.com>
2025-02-18 23:18:52 +0530

    ngo food pic list error solved

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Ensure DB_URL is set
-const mongoURI = 'mongodb://localhost:27017/foodpage';
+const mongoURI = process.env.MONGO_URI;
 if (!mongoURI) {
     console.error("Error: DB_URL is not defined in environment variables.");
     process.exit(1);
